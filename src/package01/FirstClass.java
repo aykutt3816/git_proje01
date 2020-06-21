2e6ca968e8e954e89a63033b2d20e7a38a0ae555
Aykut <ata35314@gmail.com>
2020-06-21 21:48:47 +0200

    yeni bir commit masterdan

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -6,6 +6,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("ilk commit from repository");
         System.out.println("ikinci commit from repository");
+        
+        System.out.println("masterdan israrci commit");
 	}
 
 }
